abf88940fb91c35067ba530f383dcc4034fae76e
malejandromorenov <39534212+malejandromorenov@users.noreply.github.com>
2018-12-13 12:06:39 -0300

    Update ScriptGNUCloud.sh

--- ScriptGNUCloud.sh
+++ ScriptGNUCloud.sh
@@ -1,25 +1,28 @@
 #!bin/bash
-
 echo -----------------------------------------
 echo ---
 echo RUN WITH PRECAUTION, AVAILABLE ONLY LINUX
 echo ---
 echo -----------------------------------------
-apt install ruby-thor
-apt install php-pear
-sudo apt-get install iptables-persistent
-sudo apt-get install ntp
-apt install proot
-sudo apt purge upstart
-sudo apt install upstart
-sudo reboot
-sudo apt purge upstart
-apt install nmap
-apt install nuget
-apt install php7.2-cli
+#INIT SCRPT WITH SUPERUSER
+sudo su
+#
+apt-get install ruby-thor
+#INSTALL PECL REPOSITORIES
+apt-get install php-pear
+#
+apt-get install iptables-persistent
+apt-get install ntp
+apt-get install proot
+apt-get purge upstart
+apt-get install upstart
+apt-get install nmap
+apt-get install nuget
+apt-get install php7.2-cli
 #nodejs
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
+bash nodesource_setup.sh
+#
 apt-get install gnupg2
 apt-get install golang-go
 apt-get install alpine
@@ -28,31 +31,35 @@ apt-get install alice
 apt-get install nodejs
 apt-get install yarn
 apt-get install ruby
-#GEMs only run after install Ruby
+#GEMS ONLY RUN AFTER INSTALL RUBY
 gem install gitlab-development-kit
 gem install rhc
-apt install mono-runtime
+#
+apt-get install mono-runtime
 apt-get install golang-go
 apt-get install gccgo-go
-apt install imagemagick-6.q16
-apt install graphicsmagick-imagemagick-compat
-apt install imagemagick-6.q16hdr
+apt-get install imagemagick-6.q16
+apt-get install graphicsmagick-imagemagick-compat
+apt-get install imagemagick-6.q16hdr
+#BREW IS A MACOSX REPOSITORIES
 apt-get install linuxbrew-wrapper
+#ADD TO PATH
 echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bashrc
 echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
 echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
-apt install opam
-apt install nginx
-apt install vagrant
-apt install varnish
-apt install trash-cli
-apt install python-pip
-apt install elixir
-apt install qemu
-apt install apache2-bin
+#
+apt-get install opam
+apt-get install nginx
+apt-get install vagrant
+apt-get install varnish
+apt-get install trash-cli
+apt-get install python-pip
+apt-get install elixir
+apt-get install qemu
+apt-get install apache2-bin
 apt-get install default-jdk
-sudo apt install maven
-#BD
+apt-get install maven
+#BD IS DEVELOPER FOR VARIABLES
 apt install mongodb-clients
 apt install mariadb-client-core-10.1
 apt install sqlite
@@ -63,23 +70,18 @@ apt-get update
 apt-get install -y mssql-server
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-sudo apt-get update 
 sudo apt-get install mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-apt-get update 
+#
 apt-get install mssql-server-agent
-apt-get update 
 apt-get install -y mssql-server-fts
 apt-get install mssql-server-is
-sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 #Download appropriate package for the OS version
 #Choose only ONE of the following, corresponding to your OS version
 #Ubuntu 18.04
 curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install mssql-tools
@@ -87,15 +89,14 @@ echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install unixodbc-dev
+apt-get install unixodbc-dev
 #.net
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1
-#Docker
-sudo apt-get install apt-transport-https
+apt-get install apt-transport-https
+apt-get install dotnet-sdk-2.1
+#DOCKER CONTAINER 
+apt-get install apt-transport-https
 apt-get install ca-certificates   
 apt-get install curl   
 apt-get install software-properties-common 
@@ -104,17 +105,17 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce
-sudo docker run hello-world
-#Cloud
-#Azure
+apt-get install docker-ce
+docker run hello-world
+#CLOUD INIT NOVEMBER 2017
+#AZURE
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
 sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install apt-transport-https azure-cli
-#IBM
+#IBMCLOUD
 curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
 ibmcloud plugin repo-plugins -r 'IBM Cloud'
 ibmcloud plugin install cloud-object-storage
@@ -135,7 +136,7 @@ ibmcloud plugin install container-service/kubernetes-service
 ibmcloud plugin install private-network-peering
 ibmcloud plugin install vpn
 ibmcloud plugin install auto-scaling
-#Google
+#GOOGLECLOUD
 # Create environment variable for correct distribution
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 # Add the Cloud SDK distribution URI as a package source
@@ -163,7 +164,7 @@ apt-get install google-cloud-sdk-cloud-datastore-emulator
 apt-get install google-cloud-sdk-datalab
 apt-get install google-cloud-sdk-app-engine-go
 sudo apt-get install google-cloud-sdk-cbt
-#Puppet
+#PPUPPET
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt update
@@ -173,16 +174,8 @@ sudo apt-get update
 sudo apt-get install pdk
 #AWS
 pip install awscli --upgrade --user
-#brew run without sudo
-brew install gcc
-brew install git
-brew install go
-brew install gpg
-brew install nodejs
-brew install elixir
-brew install qemu
 #Update December 2018
-#rust
+#RUST
 curl https://sh.rustup.rs -sSf | sh
 #LLVM
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
@@ -193,3 +186,15 @@ apt-get install clang-6.0 clang-tools-6.0 clang-6.0-doc libclang-common-6.0-dev 
 apt-get install libfuzzer-6.0-dev
 apt-get install lldb-6.0
 apt-get install lld-6.0
+#
+#
+#BREW RUN WITOUTH SUDO
+exit
+#
+brew install gcc
+brew install git
+brew install go
+brew install gpg
+brew install nodejs
+brew install elixir
+brew install qemu
