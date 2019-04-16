2a6cbf4689d26bc9f7f1a4202de809673f908b4d
malejandromorenov <39534212+malejandromorenov@users.noreply.github.com>
2019-04-16 07:54:18 -0400

    Update scriptubuntu.sh

--- scriptubuntu.sh
+++ scriptubuntu.sh
@@ -7,114 +7,114 @@ echo -----------------------------------------
 #INIT SCRPT WITH SUPERUSER
 sudo su
 #
-apt-get install ruby-thor
+sudo su apt-get install ruby-thor
 #INSTALL PECL REPOSITORIES
-apt-get install php-pear
+sudo su apt-get install php-pear
 #
-apt-get install iptables-persistent
-apt-get install ntp
-apt-get install proot
-apt-get purge upstart
-apt-get install upstart
-apt-get install nmap
-apt-get install nuget
-apt-get install php7.2-cli
+sudo su apt-get install iptables-persistent
+sudo su apt-get install ntp
+sudo su apt-get install proot
+sudo su apt-get purge upstart
+sudo su apt-get install upstart
+sudo su apt-get install nmap
+sudo su apt-get install nuget
+sudo su apt-get install php7.2-cli
 #nodejs
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-bash nodesource_setup.sh
-#
-apt-get install gnupg2
-apt-get install golang-go
-apt-get install alpine
-apt-get install kleopatra
-apt-get install alice
-apt-get install nodejs
-apt-get install yarn
-apt-get install ruby
+sudo su curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo su bash nodesource_setup.sh
+#Basic 
+sudo su apt-get install gnupg2
+sudo su apt-get install golang-go
+sudo su apt-get install alpine
+sudo su apt-get install kleopatra
+sudo su apt-get install alice
+sudo su apt-get install nodejs
+sudo su apt-get install yarn
+sudo su apt-get install ruby
 #GEMS ONLY RUN AFTER INSTALL RUBY
-gem install gitlab-development-kit
-gem install rhc
+sudo su gem install gitlab-development-kit
+sudo su gem install rhc
 #
-apt-get install mono-runtime
-apt-get install golang-go
-apt-get install gccgo-go
-apt-get install imagemagick-6.q16
-apt-get install graphicsmagick-imagemagick-compat
-apt-get install imagemagick-6.q16hdr
+sudo su apt-get install mono-runtime
+sudo su apt-get install golang-go
+sudo su apt-get install gccgo-go
+sudo su apt-get install imagemagick-6.q16
+sudo su apt-get install graphicsmagick-imagemagick-compat
+sudo su apt-get install imagemagick-6.q16hdr
 #BREW IS A MACOSX REPOSITORIES
-apt-get install linuxbrew-wrapper
+sudo su apt-get install linuxbrew-wrapper
 #ADD TO PATH
 echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bashrc
 echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
 echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
 #
-apt-get install opam
-apt-get install nginx
-apt-get install vagrant
-apt-get install varnish
-apt-get install trash-cli
-apt-get install python-pip
-apt-get install elixir
-apt-get install qemu
-apt-get install apache2-bin
-apt-get install default-jdk
-apt-get install maven
+sudo su apt-get install opam
+sudo su apt-get install nginx
+sudo su apt-get install vagrant
+sudo su apt-get install varnish
+sudo su apt-get install trash-cli
+sudo su apt-get install python-pip
+sudo su apt-get install elixir
+sudo su apt-get install qemu
+sudo su apt-get install apache2-bin
+sudo su apt-get install default-jdk
+sudo su apt-get install maven
 #BD IS DEVELOPER FOR VARIABLES
-apt install mongodb-clients
-apt install mariadb-client-core-10.1
-apt install sqlite
+sudo su apt install mongodb-clients
+sudo su apt install mariadb-client-core-10.1
+sudo su apt install sqlite
 #SQL 2017
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
-apt-get update
-apt-get install -y mssql-server
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-sudo apt-get install mssql-tools unixodbc-dev
+sudo su wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo su add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
+sudo su apt-get update
+sudo su apt-get install -y mssql-server
+sudo su curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo su curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo su apt-get install mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 #
-apt-get install mssql-server-agent
-apt-get install -y mssql-server-fts
-apt-get install mssql-server-is
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+sudo su apt-get install mssql-server-agent
+sudo su apt-get install -y mssql-server-fts
+sudo su apt-get install mssql-server-is
+sudo su curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 #Download appropriate package for the OS version
 #Choose only ONE of the following, corresponding to your OS version
 #Ubuntu 18.04
-curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo su curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo su  ACCEPT_EULA=Y apt-get install msodbcsql17
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-apt-get install unixodbc-dev
+sudo su apt-get install unixodbc-dev
 #.net
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-apt-get install apt-transport-https
-apt-get install dotnet-sdk-2.1
+sudo su wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo su  dpkg -i packages-microsoft-prod.deb
+sudo su apt-get install apt-transport-https
+sudo su apt-get install dotnet-sdk-2.1
 #DOCKER CONTAINER 
-apt-get install apt-transport-https
-apt-get install ca-certificates   
-apt-get install curl   
-apt-get install software-properties-common 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+sudo su apt-get install apt-transport-https
+sudo su apt-get install ca-certificates   
+sudo su apt-get install curl   
+sudo su apt-get install software-properties-common 
+sudo su curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo su add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get install docker-ce
-docker run hello-world
+sudo su apt-get install docker-ce
+sudo su docker run hello-world
 #CLOUD INIT NOVEMBER 2017
 #AZURE
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
 sudo tee /etc/apt/sources.list.d/azure-cli.list
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install apt-transport-https azure-cli
+sudo su curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo su sudo apt-get update
+sudo su apt-get install apt-transport-https azure-cli
 #IBMCLOUD
 curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
 ibmcloud plugin repo-plugins 'IBM Cloud'
@@ -150,49 +150,49 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 RUN export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
     echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
-    apt-get update -y && apt-get install google-cloud-sdk -y
-apt-get install google-cloud-sdk-app-engine-python-extras
-apt-get install google-cloud-sdk-app-engine-python
-apt-get install google-cloud-sdk-app-engine-php
-apt-get install google-cloud-sdk-app-engine-java
-apt-get install google-cloud-sdk-docker-credential-gcr
-apt-get install google-cloud-sdk-container-builder-local
-apt-get install google-cloud-sdk-cloud-build-local
-apt-get install google-cloud-sdk-emulator-reverse-proxy
-apt-get install google-cloud-sdk-cloud_sql_proxy
-apt-get install google-cloud-sdk-pubsub-emulator
-apt-get install google-cloud-sdk-gcd-emulator
-apt-get install google-cloud-sdk-cloud-datastore-emulator
-apt-get install google-cloud-sdk-datalab
-apt-get install google-cloud-sdk-app-engine-go
-sudo apt-get install google-cloud-sdk-cbt
+    sudo su curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+    sudo su apt-get update -y && apt-get install google-cloud-sdk -y
+sudo su apt-get install google-cloud-sdk-app-engine-python-extras
+sudo su apt-get install google-cloud-sdk-app-engine-python
+sudo su apt-get install google-cloud-sdk-app-engine-php
+sudo su apt-get install google-cloud-sdk-app-engine-java
+sudo su apt-get install google-cloud-sdk-docker-credential-gcr
+sudo su apt-get install google-cloud-sdk-container-builder-local
+sudo su apt-get install google-cloud-sdk-cloud-build-local
+sudo su apt-get install google-cloud-sdk-emulator-reverse-proxy
+sudo su apt-get install google-cloud-sdk-cloud_sql_proxy
+sudo su apt-get install google-cloud-sdk-pubsub-emulator
+sudo su apt-get install google-cloud-sdk-gcd-emulator
+sudo su apt-get install google-cloud-sdk-cloud-datastore-emulator
+sudo su apt-get install google-cloud-sdk-datalab
+sudo su apt-get install google-cloud-sdk-app-engine-go
+sudo su apt-get install google-cloud-sdk-cbt
 #PPUPPET
-wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
-sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo su wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+sudo su dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt update
 gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F
 gpg --list-key --fingerprint 7F438280EF8D349F
 sudo apt-get update
-sudo apt-get install pdk
+sudo su apt-get install pdk
 #AWS
 pip install awscli --upgrade --user
 #Update December 2018
 #RUST
-curl https://sh.rustup.rs -sSf | sh
+sudo su curl https://sh.rustup.rs -sSf | sh
 #LLVM
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo su wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
-apt-get install clang-6.0 lldb-6.0 lld-6.0
-apt-get install libllvm-6.0-ocaml-dev libllvm6.0 llvm-6.0 llvm-6.0-dev llvm-6.0-doc llvm-6.0-examples llvm-6.0-runtime
-apt-get install clang-6.0 clang-tools-6.0 clang-6.0-doc libclang-common-6.0-dev libclang-6.0-dev libclang1-6.0 clang-format-6.0 python-clang-6.0
-apt-get install libfuzzer-6.0-dev
-apt-get install lldb-6.0
-apt-get install lld-6.0
+sudo su apt-get install clang-6.0 lldb-6.0 lld-6.0
+sudo su apt-get install libllvm-6.0-ocaml-dev libllvm6.0 llvm-6.0 llvm-6.0-dev llvm-6.0-doc llvm-6.0-examples llvm-6.0-runtime
+sudo su apt-get install clang-6.0 clang-tools-6.0 clang-6.0-doc libclang-common-6.0-dev libclang-6.0-dev libclang1-6.0 clang-format-6.0 python-clang-6.0
+sudo su apt-get install libfuzzer-6.0-dev
+sudo su apt-get install lldb-6.0
+sudo su apt-get install lld-6.0
 #
 #CLOUD FOUNDRY
 # ...first add the Cloud Foundry Foundation public key and package repository to your system
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+sudo su wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 # ...then, update your local package index, then finally install the cf CLI
 sudo apt-get update
