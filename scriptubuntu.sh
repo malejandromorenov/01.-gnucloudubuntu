18750d3a6ec6219ccf5b1a0d6e2976d316cf021a
mauricio moreno <mauricio.morenov@mayor.cl>
2019-06-21 18:51:52 -0400

    Update scriptubuntu.sh

--- scriptubuntu.sh
+++ scriptubuntu.sh
@@ -209,6 +209,7 @@ brew install elixir
 brew install qemu
 brew install jq
 brew install gnu-go
+brew install yarn
 #Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
